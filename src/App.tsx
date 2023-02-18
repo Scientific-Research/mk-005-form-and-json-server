2370f977d0f7ba3292692003f7f77f3d644981d7
Maximilian <scientificresearch309@gmail.com>
2023-02-18 10:56:24 +0100

    form Data added and some CSS

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,42 @@
+import { useState } from "react";
 import "./App.scss";
 
+const _formData = {
+  jobTitle: "",
+  description: "",
+};
+
 function App() {
+  const [formData, setFormData] = useState(_formData);
+
   return (
     <div className="App">
       <h1>Job Site</h1>
-      <form action="">
-        <fieldset>
-          <legend>New Job</legend>
-          <div className="row">
-            <label htmlFor="">Job Title</label>
-            <div>
-              <input type="text" />
-            </div>
-          </div>
 
-          <div className="row">
-            <label htmlFor="">Description</label>
-            <div>
-              <textarea />
+      <section>
+        <form action="">
+          <fieldset>
+            <legend>New Job</legend>
+            <div className="row">
+              <label htmlFor="">Job Title</label>
+              <div>
+                <input type="text" />
+              </div>
             </div>
-          </div>
-        </fieldset>
-      </form>
+
+            <div className="row">
+              <label htmlFor="">Description</label>
+              <div>
+                <textarea />
+              </div>
+            </div>
+          </fieldset>
+        </form>
+
+        <div className="debuggingArea">
+          <pre>{JSON.stringify(formData, null, 2)}</pre>
+        </div>
+      </section>
     </div>
   );
 }
